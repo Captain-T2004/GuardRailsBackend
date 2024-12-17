972a0947756b793f173f0d3714230ba4c924df14
Akshay Behl <akshaybehl231@gmail.com>
2024-12-17 13:29:39 +0530

    Revert "Made a lot of changes"
    This reverts commit 33e8f09576f01b730d3ba611125dedb4bdb72d7c.

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy import Column, Integer, String, Text, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, create_engine
-from sqlalchemy.sql import func
-from sqlalchemy.dialects.postgresql import JSON
 
 load_dotenv()
 
@@ -28,23 +26,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    sub = Column(String, nullable=False)
-    api_key = Column(String, unique=True, nullable=False, index=True)
+    api_key = Column(String, unique=True, index=True)
     input_validators = Column(Text, nullable=False)
     output_validators = Column(Text, nullable=False)
-    selected_model = Column(String, nullable=False)
-    sessions = relationship("Session", back_populates="user")
-
-class Session(Base):
-    __tablename__ = "sessions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    session_id = Column(String, unique=True, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    user = relationship("User", back_populates="sessions")
-    start_time = Column(DateTime(timezone=True), server_default=func.now())
-    end_time = Column(DateTime(timezone=True), nullable=True)
-    inputs = Column(JSON, default=[])
-    outputs = Column(JSON, default=[])
 
 Base.metadata.create_all(bind=engine)
--- models.py
+++ models.py
@@ -6,7 +6,3 @@ class ValidationRequest(BaseModel):
 class RegistrationRequest(BaseModel):
     input_validators: list[str]
     output_validators: list[str]
-    selected_model: str
-
-class KeyDeletionRequest(BaseModel):
-    key_id: str
